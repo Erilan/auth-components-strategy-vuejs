5a9b19cc6157987681588e72027d79f28f381829
Antonin Savoie <asavoie@kaliop.com>
2018-08-20 17:19:50 +0200

    improve router

--- src/components/TheLogin.vue
+++ src/components/TheLogin.vue
@@ -22,12 +22,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  margin: 40px 0 0;
-  color: blue;
-}
-
-</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import PrivatePage from '../components/PrivatePage'
+import PrivatePage from './components/PrivatePage'
 
 Vue.use(Router)
 
